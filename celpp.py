2baa9c4a156731c05e958fb03276e70024076bdf
Natalie Rittenhouse <NRittenhouse@PoholekLabs-MacBook-Pro.local>
2017-02-19 22:40:47 -0500

    Added upload function

--- celpp.py
+++ celpp.py
@@ -8,12 +8,12 @@ class _main_():
 	
 	
 	def fetchData():
-		if os.path.isdir(os.path.expanduser("~/Desktop/celpp")) == False: #creates celpp folder on users desktop if not already there
-			os.mkdir(os.path.expanduser("~/Desktop/celpp"))
+		if os.path.isdir(os.path.expanduser("~/Desktop/celpp_data_data")) == False: #creates celpp_data folder on users desktop if not already there
+			os.mkdir(os.path.expanduser("~/Desktop/celpp_data_data"))
 		else: 
-			print("All Files will go into the celpp folder")
+			print("All Files will go into the celpp_data folder")
 			
-		cred = os.path.expanduser("~/Desktop/celpp/credentials.txt")
+		cred = os.path.expanduser("~/Desktop/celpp_data_data/credentials.txt")
 		try: #attempts to connect to file required to connect to ftp
 			print("Trying to open credentials.txt")
 			fo = open(cred, 'r')
@@ -24,11 +24,11 @@ class _main_():
 			fo.write("host ftp.box.com\nuser nlr23@pitt.edu\npass h@il2pitt1\npath\ncontestantid 33824\nchallengepath /challengedata\nsubmissionpath /33824")
 			fo.close()
 		
-		if(os.path.isdir(os.path.expanduser("~/Desktop/celpp/challengedata"))==False):#creates challengedata folder if it doesn't exist
-			os.mkdir(os.path.expanduser("~/Desktop/celpp/challengedata"))
-			os.chdir(os.path.expanduser("~/Desktop/celpp/challengedata"))
+		if(os.path.isdir(os.path.expanduser("~/Desktop/celpp_data/challengedata"))==False):#creates challengedata folder if it doesn't exist
+			os.mkdir(os.path.expanduser("~/Desktop/celpp_data/challengedata"))
+			os.chdir(os.path.expanduser("~/Desktop/celpp_data/challengedata"))
 		else: #changes to challengedata folder if it exists
-			os.chdir(os.path.expanduser("~/Desktop/celpp/challengedata"))
+			os.chdir(os.path.expanduser("~/Desktop/celpp_data/challengedata"))
 			
 			
 		ftp = FtpFileTransfer(cred)
@@ -48,9 +48,9 @@ class _main_():
 					print('Deleting zip file: ' + x)
 					os.system('rm ' + x)
 				elif os.path.isdir(str(dir[0])) == True:#if it finds the unzipped directory in local folders
-					print(str(dir[0]) + ' exists')
+					pass
 				else: #if it can't find the week in any format; downloads, unzips, and removes zipped file
-					ftp.download_file('challengedata/'+ x,  os.path.expanduser("~/Desktop/celpp/challengedata") + '/' + x)
+					ftp.download_file('challengedata/'+ x,  os.path.expanduser("~/Desktop/celpp_data/challengedata") + '/' + x)
 					print('Unzipping folder ' + str(dir[0]))
 					os.system('tar -xzf ' + x)
 					print('Deleting zip file: ' + x)
@@ -58,11 +58,42 @@ class _main_():
 					count = count + 1
 					print(str(dir[0]) + " was just added to the challengedata folder")
 			else: 
-				ftp.download_file('challengedata/'+ x,  os.path.expanduser("~/Desktop/celpp/challengedata") + '/' + x)
+				ftp.download_file('challengedata/'+ x,  os.path.expanduser("~/Desktop/celpp_data/challengedata") + '/' + x)
 		print("challengedata has been updated. " + str(count) + " week(s) was/were just added.")		
 		print("Disconnecting from ftp")
 		ftp.disconnect()
+		
+	def uploadData():#uploads zip files containing docking predictions to contestant folder specified in credentials.txt
+		print("Uploading files to box contestant folder")
+		cred = os.path.expanduser("~/Desktop/celpp_data/credentials.txt")
+		ftp = FtpFileTransfer(cred)
+		print("Connecting to ftp.box.com")
+		ftp.connect() 
+		print("Connected to ftp")
+		ftp_files = ftp.list_files(ftp.get_contestant_id())#creates list of files from box
+		d = []
+		for(_, dirnames, _) in os.walk(os.path.expanduser("~/Desktop/celpp_data/protocols")):
+			d.extend(dirnames)
+			break
+		for dir in (d):#NOT A FAN OF THE NESTED FOR LOOPS, WILL TRY TO FIND BETTER WAY TO IMPLEMENT THIS
+			for(_,_,filenames) in os.walk(os.path.expanduser("~/Desktop/celpp_data/protocols/" + dir)):
+				f = []
+				f.extend(filenames)
+				print("Uploading files for " + dir)
+				for x in (f): 
+					if((x in ftp_files) == False):
+						file = os.path.expanduser("~/Desktop/celpp_data/protocols/" + dir + "/" + x)
+						remote_dir = ftp.get_remote_submission_dir()
+						remote_file_name = os.path.basename(file)
+						ftp.upload_file_direct(file, remote_dir, remote_file_name)
+					else:
+						pass	
+		print("All the files have been uploaded. Disconnecting from ftp")
+		ftp.disconnect()
+		
+		
 	
 	fetchData()
+	#uploadData()
 
 
