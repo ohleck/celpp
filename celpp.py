506d58f76dab2d5d5d071fd02092bb821a749945
Ryan Jansson <ryj4@pitt.edu>
2017-04-05 12:34:18 -0400

    updated

--- celpp.py
+++ celpp.py
@@ -69,18 +69,13 @@ class _main_():
 		print('challengedata has been updated. ' + str(count) + ' week(s) was/were just added.')		
 		print('Disconnecting from ftp')
 		ftp.disconnect()
-		
-		
-	
 	
 	def	align():
 		
-		global wd 
-		
+		global wd 	
 		ans = wd +'/challengedata/answers'
 		if os.path.isdir(ans)==False: #if the answers directory isnt formed make it
-			os.mkdir(wd+'/challengedata/answers')
-		
+			os.mkdir(wd+'/challengedata/answers')	
 		data = os.listdir(wd+'/challengedata')
 		for x in (data):#for each weeks data
 			if x=="readme.txt" or x=="latest.txt" or x=="answers" : 
@@ -88,83 +83,71 @@ class _main_():
 			else:
 				toDir = wd +'/challengedata/answers/' + x
 				if os.path.isdir(toDir)==False: #if the path to answers dir doesnt exist 
-					os.mkdir(toDir) #make directory
-					#dock=os.listdir(wd+'/'+x)
-					#print(dock)
-					
-					
-				else:
-					dock=os.listdir(wd+'/challengedata/'+x)
-					for y in (dock):
-						if y=='readme.txt' or y=='new_release_structure_sequence_canonical.tsv' or y == 'new_release_structure_nonpolymer.tsv' or y=='new_release_crystallization_pH.tsv' or y=='new_release_structure_sequence.tsv':
-							pass
-						else:
-							
-							input = os.listdir(wd+'/challengedata/'+x+'/'+y)
-							for z in (input):
-								if z.startswith("LMCSS") and z.endswith(".pdb"):
-									if(z.endswith("lig.pdb")):
-										pass
-									else:
-										sts = str("grep ATOM "+ z+" > lmcss_rec.pdb")
-										wd = os.getcwd()
-										os.chdir(wd+'/challengedata/'+x+'/'+y)
-										os.system(sts)
-										os.chdir(wd)
-										input = os.listdir(wd+'/challengedata/'+x+'/'+y)
-										for z in (input):
-											if z.endswith(".smi"): #this is where weird issue/output arises...only with some input
-												wd = str(os.getcwd())
-												#print(z)
-												sts = str(" "+wd+'/challengedata/'+x+'/'+y+'/'+z +" lig.sdf --maxconfs 1")
-												os.chdir(wd+'/challengedata/'+x+'/'+y)
-												os.system(wd+'/rdkit-scripts/rdconf.py'+ sts)
-												os.chdir(wd)
-										input = os.listdir(wd+'/challengedata/'+x+'/'+y)
-										for z in (input):
-											if z.endswith("lig.pdb"):
-												sts=str("smina -r lmcss_rec.pdb -l lig.sdf --autobox_ligand "+z+" -o lmcss_docked.sdf")
-												wd=str(os.getcwd())
-												os.chdir(wd+'/challengedata/'+x+'/'+y)
-												os.system(sts)
-												os.chdir(wd+'/challengedata/')
-											#access denied
-										input = os.listdir(wd+'/challengedata/'+x+'/'+y)		
-										#for z in (input):
-										#	if(z=='lmcss_docked.sdf'):
-										#		break
-												#wd = str(os.getcwd())							
-											#	sts=str(wd+"/challengedata/"+x+'/'+y+'/'+ " sdsorter lmcss_docked.sdf -print")
-								#				os.system(sts)
-								#			os.chdir(wd)
-										cur = str(os.getcwd()+'/answers/'+x+'/'+y)
-										if (os.path.isdir(cur)==True):
-											os.chdir(wd+'/challengedata/'+x+'/'+y)
-											input = os.listdir(wd+'/challengedata/'+x+'/'+y)
-											for i in (input):
-												if i.endswith("lig.pdb"):
-													sts=str("obrms -f "+i+" lmcss_docked.sdf")
-													os.system(sts)
-													curdir = str(wd+'/challengedata/'+x+'/'+y+'/lmcss_docked.sdf')
-													todir = str(wd+'/challengedata/answers/'+x+'/'+y+'/')
-													shutil.copy2(curdir, todir)
-													break
-											os.chdir(wd)
-										if(os.path.isdir(cur)==False):
-											os.mkdir(cur)
-											os.chdir(wd+'/challengedata/'+x+'/'+y)
-											input = os.listdir(wd+'/challengedata/'+x+'/'+y)
-											for i in (input):
-												if i.endswith("lig.pdb"):
-													sts=str("obrms -f "+i+" lmcss_docked.sdf")
-													os.system(sts)				
-													curdir = str(wd+'/challengedata/'+x+'/'+y+'/lmcss_docked.sdf')
-													todir = str(wd+'/challengedata/answers/'+x+'/'+y+'/')
-													shutil.copy2(curdir, todir)
-													break
+					os.mkdir(toDir) #make directory					
+				dock=os.listdir(wd+'/challengedata/'+x)
+				for y in (dock):
+					a = str(os.getcwd()+'/answers/'+x+'/'+y+'/lmcss_docked.sdf')
+					if y=='readme.txt' or y=='new_release_structure_sequence_canonical.tsv' or y == 'new_release_structure_nonpolymer.tsv' or y=='new_release_crystallization_pH.tsv' or y=='new_release_structure_sequence.tsv':
+						pass
+					elif os.path.isfile(a)==True:
+						pass
+					else:
+						input = os.listdir(wd+'/challengedata/'+x+'/'+y)
+						for z in (input):
+							if z.startswith("LMCSS") and z.endswith(".pdb"):
+								if(z.endswith("lig.pdb")):
+									pass
+								else:
+									sts = str("grep ATOM "+ z+" > lmcss_rec.pdb")
+									wd = os.getcwd()
+									os.chdir(wd+'/challengedata/'+x+'/'+y)
+									os.system(sts)
 									os.chdir(wd)
-								
-				
+									input = os.listdir(wd+'/challengedata/'+x+'/'+y)
+									for z in (input):
+										if z.endswith(".smi"): #this is where weird issue/output arises...only with some input
+											wd = str(os.getcwd())
+											#print(z)
+											sts = str(" "+wd+'/challengedata/'+x+'/'+y+'/'+z +" lig.sdf --maxconfs 1")
+											os.chdir(wd+'/challengedata/'+x+'/'+y)
+											os.system(wd+'/rdkit-scripts/rdconf.py'+ sts)
+											os.chdir(wd)
+									input = os.listdir(wd+'/challengedata/'+x+'/'+y)
+									for z in (input):
+										if z.endswith("lig.pdb"):
+											sts=str("smina -r lmcss_rec.pdb -l lig.sdf --autobox_ligand "+z+" -o lmcss_docked.sdf")
+											wd=str(os.getcwd())
+											os.chdir(wd+'/challengedata/'+x+'/'+y)
+											os.system(sts)
+											os.chdir(wd+'/challengedata/')
+									cur = str(os.getcwd()+'/answers/'+x+'/'+y)
+									if (os.path.isdir(cur)==True):
+										os.chdir(wd+'/challengedata/'+x+'/'+y)
+										input = os.listdir(wd+'/challengedata/'+x+'/'+y)
+										for i in (input):
+											if i.endswith("lig.pdb"):
+												sts=str("obrms -f "+i+" lmcss_docked.sdf")
+												os.system(sts)
+												curdir = str(wd+'/challengedata/'+x+'/'+y+'/lmcss_docked.sdf')
+												todir = str(wd+'/challengedata/answers/'+x+'/'+y+'/')
+												shutil.copy(curdir, todir)
+												print(curdir)
+												break
+										os.chdir(wd)
+									else:
+										os.mkdir(cur)
+										os.chdir(wd+'/challengedata/'+x+'/'+y)
+										input = os.listdir(wd+'/challengedata/'+x+'/'+y)
+										for i in (input):
+											if i.endswith("lig.pdb"):
+												sts=str("obrms -f "+i+" lmcss_docked.sdf")
+												os.system(sts)				
+												curdir = str(wd+'/challengedata/'+x+'/'+y+'/lmcss_docked.sdf')
+												todir = str(wd+'/challengedata/answers/'+x+'/'+y+'/')
+												shutil.copy(curdir, todir)
+												print(curdir)
+												break
+								os.chdir(wd)				
 					filename = toDir + ".zip"
 					if os.path.isfile(filename): #if the zipped answers already exist, skip
 						pass
@@ -228,5 +211,3 @@ class _main_():
 	align()
 	#compare()
 	#uploadData()
-
-
